e98af3f929d6c5e59c072553c1ca5065ad795eb2
Andrey Smirnoff <smirnoffa675@gmail.com>
2024-12-29 17:03:33 +0100

    main

--- assets/services/mail.service.js
+++ assets/services/mail.service.js
@@ -1,10 +1,11 @@
 import nodemailer from 'nodemailer'; 
 import * as fs from 'fs'; 
 import juice from 'juice'; 
-import senderAccounts from '../db/emails.json' with { type: "json" }; 
-import emails from '../db/clients.json' with { type: "json" };
+import senderAccounts from '../db/emails.json' with {type: "json"}
+import emails from '../db/clients.json' with {type: "json"}
 
-let emailCounter = 0; let senderIndex = 0; 
+let emailCounter = 0; 
+let senderIndex = 0; 
 
 function createTransporter(account) {
     if (!account || !account.email || !account.password) { 
@@ -12,27 +13,43 @@ function createTransporter(account) {
         throw new Error('Отсутствуют учетные данные для аутентификации');
     }
 
-    return nodemailer.createTransport({ host: 'smtp.timeweb.ru', port: 465, secure: true, auth: { user: account.email, pass: account.password},
+    return nodemailer.createTransport({
+        host: 'smtp.timeweb.ru',
+        port: 465,
+        secure: true,
+        auth: { user: account.email, pass: account.password },
     });
-
 }
 
 export async function sendEmails() { 
     try { 
-          const htmlContent = juice(fs.readFileSync("./index.html", 'utf8')); for (let i = 0; i < emails.length; i++) { 
-          if (emailCounter % 2000 === 0 && emailCounter !== 0) {
-                senderIndex = (senderIndex + 1) % senderAccounts.length; console.log(`Сменился отправитель: ${senderAccounts[senderIndex].email}`);
+        const htmlContent = fs.readFileSync("./index.html", 'utf8');
+        const inlineHtml = juice(htmlContent);  // Встраиваем стили
+
+        for (let i = 0; i < emails.length; i++) { 
+            if (emailCounter % 2000 === 0 && emailCounter !== 0) {
+                senderIndex = (senderIndex + 1) % senderAccounts.length; 
+                console.log(`Сменился отправитель: ${senderAccounts[senderIndex].email}`);
             }
-          console.log(`Используется отправитель: ${JSON.stringify(senderAccounts[senderIndex])}`); const transporter = createTransporter(senderAccounts[senderIndex]); 
-          const mailOptions = {
-                from: senderAccounts[senderIndex].email, to: emails[i].email, subject: "custom", html: htmlContent,
+
+            console.log(`Используется отправитель: ${JSON.stringify(senderAccounts[senderIndex])}`); 
+            const transporter = createTransporter(senderAccounts[senderIndex]); 
+            
+            const mailOptions = {
+                from: senderAccounts[senderIndex].email,
+                to: emails[i].email,
+                subject: "custom",
+                html: inlineHtml,  // Используем преобразованный HTML
             }
-          await transporter.sendMail(mailOptions); console.log(`Письмо отправлено на ${emails[i].email}`); emailCounter++; await new Promise((resolve) => 
-          setTimeout(resolve, 100));
+
+            await transporter.sendMail(mailOptions);
+            console.log(`Письмо отправлено на ${emails[i].email}`); 
+            emailCounter++; 
+            await new Promise((resolve) => setTimeout(resolve, 100));
         }
+
         console.log('Все письма отправлены.');
     } catch (error) {
         console.error('Ошибка при отправке письма:', error);
     }
 }
-
--- index.js
+++ index.js
@@ -5,9 +5,12 @@ import path from 'path';
 import { sendEmails } from "./assets/services/mail.service.js"; 
 import fetch from 'node-fetch'; 
 import { parseExcel } from "./assets/scripts/parseExcel.js"; 
+import { changeText } from "./assets/scripts/admin.js";
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname); 
+
 dotenv.config();
+
 const bot = new TelegramBot(process.env.TOKEN, { polling: true }); 
 const UPLOAD_DIR = path.join(__dirname, 'assets', 'db'); 
 
@@ -16,14 +19,21 @@ if (!fs.existsSync(UPLOAD_DIR)) {
 }
 
 bot.on('message', async msg => { 
+
     const chatId = msg.chat.id; 
+
     if (msg.text === "/start") { 
         await bot.sendMessage(chatId, "Привет! Я готов работать. Используйте команды:\n\n/start - приветствие\n/file - отправьте Excel-файл, и я его сохраню."); 
         await sendEmails();
     } else if (msg.text === "/file") {
         await bot.sendMessage(chatId, "Отправьте мне Excel-файл, и я его сохраню.");
+    } else if (msg.text === "/text"){
+        await changeText(bot, chatId)
     }
 });
+
+
+
 bot.on('document', async (msg) => { 
     const chatId = msg.chat.id; const fileName = msg.document?.file_name || 'unknown.xlsx'; 
     const fileExtension = path.extname(fileName).toLowerCase(); 
